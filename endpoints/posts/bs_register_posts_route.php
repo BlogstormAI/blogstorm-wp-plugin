81e0d89179aaed3eb68bf2812aa84034a62c1bc3
codix <bishwasbh@gmail.com>
2023-09-08 04:59:57 +0545

    Image upload fixed.

--- endpoints/posts/bs_register_posts_route.php
+++ endpoints/posts/bs_register_posts_route.php
@@ -12,7 +12,7 @@ function bs_register_post_routes(): void
 
     register_rest_route('blogstorm/v1', 'posts/create', array(
         'methods' => 'POST',
-        'callback' => bs_auth_required('blogstorm_create_post') // Include bs_auth_required wrapper
+        'callback' => bs_auth_required('blogstorm_get_or_create_post') // Include bs_auth_required wrapper
     ));
 
 
--- endpoints/posts/posts.php
+++ endpoints/posts/posts.php
@@ -43,48 +43,100 @@ function blogstorm_get_posts($request): array
 }
 
 // POST endpoint for creating a new post
-function blogstorm_create_post($request): WP_Error|array
+function blogstorm_get_or_create_post($request): WP_Error|array
 {
+    $post_id = $request['post_id'];
     $title = sanitize_text_field($request['title']);
     $content = wp_kses_post($request['content']);
     $excerpt = sanitize_text_field($request['excerpt']);
-    $featured_image_url = esc_url($request['featured_image_url']);
-    $categories = $request['categories']; // An array of category IDs
-    $tags = $request['tags']; // An array of tag IDs
+    $featured_image_url = $request['featured_image_url'];
+    $categories = $request['categories'];
+    $tags = $request['tags'];
+    $post_status = $request['post_status'];
 
+    if ($post_id) {
+        $existing_post = get_post($post_id);
+
+        if ($existing_post && $existing_post->post_type === 'post') {
+            $updated_post = array(
+                'ID' => $post_id,
+                'post_title' => $title,
+                'post_content' => $content,
+                'post_excerpt' => $excerpt,
+                'post_status' => $post_status ?: 'publish',
+            );
+
+            $post_id = wp_update_post($updated_post);
+
+            if (is_wp_error($post_id)) {
+                return new WP_Error('error', 'Failed to update the existing post');
+            }
+
+            if ($featured_image_url) {
+                error_log("featured image url: " . $featured_image_url);
+                $featured_image = media_sideload_image($featured_image_url, $existing_post, $title, 'id');
+                error_log("featured image posted.");
+                if ($featured_image) {
+                    set_post_thumbnail($existing_post, $featured_image);
+                    error_log("featured image set.");
+                }
+            }
+
+            wp_set_post_categories($post_id, $categories);
+            error_log("categories set.");
+            wp_set_post_tags($post_id, $tags);
+            error_log("tags set.");
+
+            return array(
+                'message' => 'Post updated successfully',
+                'post_id' => $post_id,
+                'post_url' => get_permalink($post_id),
+                'post_status' => get_post_status($post_id)
+            );
+        }
+    }
+
+    // If no existing post with the provided ID or if an ID is not provided, create a new post
     $new_post = array(
         'post_title' => $title,
         'post_content' => $content,
         'post_excerpt' => $excerpt,
         'post_type' => 'post',
-        'post_status' => 'publish',
+        'post_status' => $post_status ?: 'publish',
     );
 
     $post_id = wp_insert_post($new_post);
 
-    $featured_image = media_sideload_image($featured_image_url, $post_id, $title, 'id');
+    if (is_wp_error($post_id)) {
+        return new WP_Error('error', 'Failed to create a new post');
+    }
 
-    if (!is_wp_error($post_id)) {
-        // Set featured image
+    // Set featured image, categories, and tags for the new post
+    if ($featured_image_url) {
+        $featured_image = media_sideload_image($featured_image_url, $post_id, $title, 'id');
+
         if ($featured_image) {
             set_post_thumbnail($post_id, thumbnail_id: $featured_image);
         }
-
-        // Set categories and tags
-        wp_set_post_categories($post_id, $categories);
-        wp_set_post_tags($post_id, $tags);
-
-        return array('message' => 'Post created successfully', 'post_id' => $post_id, 'post_url' => get_permalink($post_id));
-    } else {
-        return new WP_Error('error', 'Failed to create a new post');
     }
+
+    wp_set_post_categories($post_id, $categories);
+    wp_set_post_tags($post_id, $tags);
+
+    return array(
+        'message' => 'Post created successfully',
+        'post_id' => $post_id,
+        'post_url' => get_permalink($post_id),
+        'post_status' => get_post_status($post_id)
+    );
 }
+
 
 function blogstorm_get_post_by_id($request): WP_Error|array
 {
-    $post_id = isset($request['id']) ? absint($request['id']) : 0;
+    $post_id = isset($request['id']) ? absint($request['id']) : null;
 
-    if ($post_id > 0) {
+    if ($post_id) {
         $post = get_post($post_id);
 
         if ($post) {
